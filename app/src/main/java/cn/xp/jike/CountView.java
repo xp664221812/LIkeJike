f35f53ca1b6c7c99598eaacd04f0c98ca56c13e1
xp664221812 <realxp141414@163.com>
2018-04-09 20:42:26 +0800

    xxx

--- app/src/main/java/cn/xp/jike/CountView.java
+++ app/src/main/java/cn/xp/jike/CountView.java
@@ -1,6 +1,5 @@
 package cn.xp.jike;
 
-import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
@@ -9,9 +8,6 @@ import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.LinearInterpolator;
-
-import java.lang.reflect.Array;
 
 
 public class CountView extends View {
@@ -19,8 +15,8 @@ public class CountView extends View {
 
     private Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
-    int transparentColor = 0xffffff;
-    int originColor = 0xff0000;
+    int transparentColor = 0xffffffff;
+    int originColor = 0xff000000;
 
     boolean plus;
     public int count;
@@ -29,24 +25,28 @@ public class CountView extends View {
 
     public int changeNum = 0;
 
-    private Point[] points = new Point[3];
+    boolean isFirstInit = true;
+
+    private Point[] points = new Point[4];
 
     public void setCount(int count) {
         this.count = count;
+        isFirstInit = true;
         Log.d(TAG, "3333333333333333333");
-        initData();
-        postInvalidate();
+//        initData();
+//        postInvalidate();
     }
 
     public void setAction(int action) {
         this.action = action;
 
         postInvalidate();
-//        invalidate();
     }
 
 
     public void setThumbNumber(float arg) {
+        Log.d(TAG, "动画来了");
+
         postInvalidate();
     }
 
@@ -69,17 +69,17 @@ public class CountView extends View {
 
     }
 
-    @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        Log.d(TAG, "2222222222222222222");
-        if (points.length == 1) {
-            Log.d(TAG, "111111111111111111");
-            Point point = points[0];
-            canvas.drawText(point.conten, point.x, point.y, paint);
-        }
 
-//        refreshView(canvas);
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Log.d(TAG, "count=================" + count);
+        if (isFirstInit) {
+            paint.setColor(originColor);
+            canvas.drawText(String.valueOf(count), 0, 120, paint);
+        } else {
+
+        }
     }
 
     public void refreshView(Canvas canvas) {
@@ -131,6 +131,7 @@ public class CountView extends View {
     }
 
     public void playPlusAnimation(boolean thumbUp) {
+        isFirstInit = false;
         plus = thumbUp;
 
 //        ObjectAnimator animator1 = ObjectAnimator.ofFloat(this, "translationY", 0, -50);
@@ -152,7 +153,7 @@ public class CountView extends View {
 //                .before(animator5).with(animator6).before(animator7);
 //        animatorSet.start();
 
-
+        calculatePoints(true);
         ObjectAnimator animator = ObjectAnimator.ofFloat(this, "thumbNumber", 0f, 0.5f);
         animator.start();
 
@@ -162,30 +163,42 @@ public class CountView extends View {
 
     private void initData() {
         Point point1 = new Point(0, 120);
-        point1.conten = String.valueOf(count);
+        point1.content = String.valueOf(count);
         point1.color = originColor;
         points[0] = point1;
-        Point point2 = new Point(, 120);
-        point1.conten = String.valueOf(count);
+        Point point2 = new Point(0, 120);
+        point1.content = String.valueOf(count);
         point1.color = originColor;
         points[0] = point1;
         Point point3 = new Point(0, 120);
-        point1.conten = String.valueOf(count);
+        point1.content = String.valueOf(count);
         point1.color = originColor;
         points[0] = point1;
         Log.d(TAG, "length===============" + points.length);
     }
 
 
-    private void caculatePoints(boolean plus) {
+    private void calculatePoints(boolean plus) {
         newCount = plus ? count + 1 : count - 1;
         char[] newArray = String.valueOf(newCount).toCharArray();
         char[] oldArray = String.valueOf(count).toCharArray();
         int num = 0;
         if (newArray.length != oldArray.length) {
             num = newArray.length;
-            Point point2 = new Point();
-            Point point3 = new Point();
+            Point point0 = new Point(0, 120, "", originColor);
+
+            Point point1 = new Point(0, 120, String.valueOf(newCount), originColor);
+
+            if (newCount > count) {
+                Point point2 = new Point(0, 100, String.valueOf(count), transparentColor);
+
+                Point point3 = new Point(0, 140, String.valueOf(newCount), transparentColor);
+            } else {
+
+            }
+
+            points[0] = point0;
+
 
         } else {
             for (int i = 0; i < newArray.length; i++) {
@@ -197,6 +210,12 @@ public class CountView extends View {
 
 
         }
+
+        Point point0 = new Point(0, 120, String.valueOf(newCount), originColor);
+        points[0] = point0;
+
+        Point point1 = new Point()
+        Point point2 = new Point();
 
 
     }
--- app/src/main/java/cn/xp/jike/Point.java
+++ app/src/main/java/cn/xp/jike/Point.java
@@ -7,7 +7,7 @@ package cn.xp.jike;
 public class Point {
     public float x;
     public float y;
-    public String conten;
+    public String content;
     public int color;
 
 
@@ -20,5 +20,11 @@ public class Point {
         this.y = y;
     }
 
+    public Point(float x, float y, String content, int color) {
+        this.x = x;
+        this.y = y;
+        this.content = content;
+        this.color = color;
+    }
 
 }
